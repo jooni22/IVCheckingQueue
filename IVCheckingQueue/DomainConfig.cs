d13bf0436ccdd6296deb8d7fbba2852e71f512b3
Vlad <tsvetkov_vlad@ukr.net>
2019-03-25 16:36:45 +0200

    Initial commit

---
+++ IVCheckingQueue/DomainConfig.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IVCheckingQueue
+{
+    public class DomainConfig
+    {
+        [JsonProperty("userName")]
+        public string userName { get; set; }
+
+        [JsonProperty("userNameSecond")]
+        public string userNameSecond { get; set; }
+
+        [JsonProperty("updateTime")]
+        public int UpdateTime { get; set; }
+
+        [JsonProperty("domains")]
+        public List<string> Domains { get; set; }
+
+        public static DomainConfig FromJson(string json) => JsonConvert.DeserializeObject<DomainConfig>(json);
+
+        public string ToJson() => JsonConvert.SerializeObject(this, Formatting.Indented);
+
+        public static DomainConfig ReadFromJson(string pathFile)
+        {
+            string jsonString = System.IO.File.ReadAllText(pathFile);
+            var config = FromJson(jsonString);
+            return config;
+        }
+
+        public static void WriteToJson(DomainConfig data, string pathFile)
+        {
+            var jsonString = data.ToJson();
+            System.IO.File.WriteAllText(pathFile, jsonString);
+        }
+    }
+}
--- IVCheckingQueue/Program.cs
+++ IVCheckingQueue/Program.cs
@@ -1,15 +1,120 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IVCheckingQueue
 {
     class Program
     {
+        public static int length = 0;
+        static string domainConfigFile = @"domainConfig.json";
+        static DomainConfig domainConfig = new DomainConfig();
+        public static void FileExists(string file)
+        {
+            if (!File.Exists(file))
+            {
+                Console.WriteLine("File " + file + " not exists");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
         static void Main(string[] args)
         {
+            FileExists(domainConfigFile);
+            domainConfig = DomainConfig.ReadFromJson(domainConfigFile);
+            string sourcePath = @"source.txt";
+            FileExists(sourcePath);
+            string[] source = File.ReadAllLines(sourcePath);
+            List<string> domainList = new List<string>();
+            foreach (var line in source)
+            {
+                string a = line.Split(' ').First();
+                if (a.Length > length) length = a.Length;
+                domainList.Add(a);
+            }
+            while (true)
+            {
+                GetInfo(domainList);
+                Thread.Sleep(domainConfig.UpdateTime * 1000);
+                Console.Clear();
+            }
+        }
+        public static void AddSpaces(string line)
+        {
+            for (int i = 0; i < length - line.Length; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+        public static void GetInfo(List<string> list)
+        {
+            string ivDomain = "https://instantview.telegram.org/contest/";
+            HtmlWeb webDoc = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument driver = webDoc.Load(ivDomain);
+            List<string> myDomainsList = domainConfig.Domains;
+            var page = driver.DocumentNode.SelectSingleNode(".//*[@class=\"list-group-contest-rows\"]");
+            foreach (var element in list)
+            {
+                var elementNode = page.SelectSingleNode($".//*[@data-domain=\"{element}\"]");
+                if (myDomainsList.Contains(element))
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.Write(element);
+                Console.ResetColor();
+                AddSpaces(element);
+                Console.Write(" - ");
+                string status = "";
+                if (elementNode == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Removed");
+                    Console.ResetColor();
+                    continue;
+                }
+                if (elementNode.SelectSingleNode($".//*[contains(@class,\"iv-deadline\")]") != null)
+                {
+                    status = elementNode.SelectSingleNode($".//*[contains(@class,\"iv-deadline\")]").InnerText;
+                }
+                else if (elementNode.SelectSingleNode($".//*[@class=\"status-winner\"]") != null)
+                {
+                    status = elementNode.SelectSingleNode($".//*[@class=\"status-winner\"]").InnerText;
+                }
+                else
+                {
+                    status = "Empty";
+                }
+                if (status == "checking")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                if (status == "Winner '19")
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                if (status == "Empty")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.Write(status);
+                Console.ResetColor();
+                Console.Write(" - ");
+                if (status != "Empty")
+                {
+                    string user = elementNode.SelectSingleNode($".//*[contains(@class,\"contest-item-candidate\")]/a").InnerText;
+                    if (user == domainConfig.userName || user == domainConfig.userNameSecond) Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(user);
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
